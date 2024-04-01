fbcd49124eb06d6aed5d804345d6a4121dfff513
enesserkam <nr.erkam@gmail.com>
2024-04-01 17:11:06 +0300

    First Commit

--- src/gguf_7b.py
+++ src/gguf_7b.py
@@ -3,7 +3,7 @@ from llama_cpp import Llama
 def main():
     # Initialize the Llama model
     llm = Llama(
-        model_path="./capybarahermes-2.5-mistral-7b.Q4_K_M.gguf",
+        model_path="../models/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf",
         n_ctx=32768,
         n_threads=12,
         n_gpu_layers=0  # Set to 0 if no GPU is available
@@ -29,3 +29,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
